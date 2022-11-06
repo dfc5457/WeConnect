0bab0c9200c22f9b516bb9346d3d4a1f532d76e2
Daniela Carannante <69178154+dfc5457@users.noreply.github.com>
2022-11-06 11:48:54 -0500

    Add files via upload

---
+++ WeConnection.java
@@ -0,0 +1,29 @@
+package weconnection;
+
+public class WeConnection {
+
+    public static void main(String[] args) {
+             
+    }
+    
+    public static void connectCanvas(String[] args) {
+        /*  Utalize Canvas API
+                Oath Authentication
+                grab course name, professor, and section 
+                grab history of course enrollments
+        */        
+    }
+
+    public static void courseGroupChats(String[] args) {
+        /* Auto create group chats based on courses
+                Utalize Discord API
+         */
+    }
+    
+    public static void hobbieGroupChats(String[] args) {
+        /* Auto create group chats based on hobbies
+                Utalize Discord API
+                Create five group chats for hobbies
+         */
+    }
+}
